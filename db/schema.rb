d8aa44e48b2527fc6de30d0f3698250efdb9393f
Kevin LeBlanc <YOUR EMAIL ADDRESS>
2018-06-20 11:57:10 -0400

    Add URL column to product table. Add index page with links to show pages.

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_06_20_144317) do
+ActiveRecord::Schema.define(version: 2018_06_20_152928) do
 
   create_table "products", force: :cascade do |t|
     t.string "name"
     t.text "description"
     t.integer "price_in_cents"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.string "url"
   end
 
 end
