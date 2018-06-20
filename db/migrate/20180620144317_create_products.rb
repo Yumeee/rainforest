914f2241a32f717d3bc2e6410d2d6e9a5be6e576
YUMEE <yumeehk@hotmail.com>
2018-06-20 15:58:09 -0400

    removed extra stuff from db/migrate

--- db/migrate/20180620144317_create_products.rb
+++ db/migrate/20180620144317_create_products.rb
@@ -6,10 +6,6 @@ class CreateProducts < ActiveRecord::Migration[5.2]
       t.integer :price_in_cents
       t.datetime :created_at
       t.datetime :updated_at
-<<<<<<< HEAD
-=======
-
->>>>>>> 9d84dc4b7c893343fe010b2441390cbe872d0dea
     end
   end
 end
